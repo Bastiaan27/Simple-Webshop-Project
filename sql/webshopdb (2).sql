10086e7783d0e778289937546461aaeffbaee4f4
Bastiaan <667382@student.inholland.nl>
2022-03-18 15:48:31 +0100

    Creation script added

--- sql/webshopdb (2).sql
+++ sql/webshopdb (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Generation Time: Jan 30, 2022 at 11:12 AM
+-- Generation Time: Mar 18, 2022 at 02:45 PM
 -- Server version: 10.6.5-MariaDB-1:10.6.5+maria~focal
 -- PHP Version: 7.4.26
 
@@ -49,44 +49,6 @@ INSERT INTO `consoletable` (`id`, `name`, `description`, `price`, `amount`, `pho
 -- --------------------------------------------------------
 
 --
--- Table structure for table `gametable`
---
-
-CREATE TABLE `gametable` (
-  `id` int(11) NOT NULL,
-  `name` varchar(60) NOT NULL,
-  `description` varchar(500) DEFAULT NULL,
-  `price` decimal(10,0) NOT NULL,
-  `amount` int(11) NOT NULL,
-  `photos` varchar(200) NOT NULL,
-  `region` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `gametable`
---
-
-INSERT INTO `gametable` (`id`, `name`, `description`, `price`, `amount`, `photos`, `region`) VALUES
-(4, 'The Legend of Zelda: Breath of the Wild', 'Breath of the Wild is an action-adventure game set in an open world where players are tasked with exploring the kingdom of Hyrule while controlling Link.', '60', 50, 'https://media.s-bol.com/R8og9o7nGGqE/76qP9DG/735x1200.jpg', 'PAL');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orders`
---
-
-CREATE TABLE `orders` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `subtotal` double NOT NULL,
-  `posted_at` datetime NOT NULL,
-  `payment_method` varchar(50) NOT NULL,
-  `items` varchar(1000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `users`
 --
 
@@ -103,8 +65,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`, `address`) VALUES
-(1, 'admin', 'admin', 'admin@gmail.com', 'adminlaan 31'),
-(2, 'test', '$2y$10$9qQ7QCYqItHGZRI2BrnZGOYfUXFVNyyJBmHfVj3Nfdm0EuOgG18yi', 'bastiaanverheul@gmail.com', 'Dijkwater, 9');
+(2, 'test', '$2y$10$9qQ7QCYqItHGZRI2BrnZGOYfUXFVNyyJBmHfVj3Nfdm0EuOgG18yi', 'bastiaanverheul@gmail.com', 'Dijkwater, 9'),
+(4, 'Mark', '$2y$10$iPtpQmghwVn5t0X3EQjeweZQQ7AHEICOnxHaogfF0KRhtYSMXEqGe', 'mark@mark.mark', 'Blijdorplaan 35');
 
 --
 -- Indexes for dumped tables
@@ -114,18 +76,6 @@ INSERT INTO `users` (`id`, `username`, `password`, `email`, `address`) VALUES
 -- Indexes for table `consoletable`
 --
 ALTER TABLE `consoletable`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `gametable`
---
-ALTER TABLE `gametable`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `orders`
---
-ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -145,22 +95,10 @@ ALTER TABLE `consoletable`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `gametable`
---
-ALTER TABLE `gametable`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `orders`
---
-ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
