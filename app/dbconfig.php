c5e6d9da95b3e735feaab5878ef14c8c591881b4
Bastiaan27 <70689135+Bastiaan27@users.noreply.github.com>
2022-04-08 17:04:21 +0200

    DB config added

--- app/dbconfig.php
+++ app/dbconfig.php
@@ -1,16 +1,21 @@
 <?php
 if (getenv('DATABASE_URL') != "") {
-    $dbopts = parse_url(getenv('DATABASE_URL'));
-    $type = "pgsql";
-    $servername = $dbopts["host"];
-    $username = $dbopts["user"];
-    $password = $dbopts["pass"];
-    $database = ltrim($dbopts["path"], '/');
+//    $dbopts = parse_url(getenv('DATABASE_URL'));
+//    $type = "pgsql";
+//    $servername = $dbopts["host"];
+//    $username = $dbopts["user"];
+//    $password = $dbopts["pass"];
+//    $database = ltrim($dbopts["path"], '/');
+    // db free hosting
+    $type = "mysql";
+    $servername = "sql11.freemysqlhosting.net";
+    $username = "sql11484545";
+    $password = "YfLaic23fH";
+    $database = "sql11484545";
 } else {
     $type = "mysql";
     $servername = "mysql";
     $username = "root";
     $password = "secret123";
-    $database = "developmentdb"/*"Cheeseshop"*/;
+    $database = "developmentdb";
 }
-?>
